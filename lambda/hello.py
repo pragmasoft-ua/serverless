20221bbc12b21b1834b797f045d508c82413d73d
Dmytro Zemnytskyi (pragmasoft) <pragmasoft@gmail.com>
2020-07-14 18:32:34 +0300

    s3 bucket, boto3 s3 client

--- lambda/hello.py
+++ lambda/hello.py
@@ -1,19 +1,35 @@
 import json
 import numpy as np
+import boto3
+import boto3_type_annotations.s3 as s3
+import os
+
+s3_client: s3.Client = None
 
 def handler(event, context):
     '''
     AWS Lambda handler
     '''
     print(f'request: {json.dumps(event)}')
-    np.random.seed(1000)
-    array = np.random.randint(0,100,100)
+    arr = np.random.randint(0,100,100)
+    file_name = '/tmp/rand.npy'
+    np.save(file_name, arr)
+    global s3_client
+    if s3_client is None:
+        s3_client = boto3.client('s3')
+        np.random.seed(1000)
+    bucket_name = os.environ['DATA_BUCKET']
+    print(f'save to bucket {bucket_name}')
+    s3_client.upload_file(file_name, bucket_name, 'rand.npy')
     return {
         'statusCode': 200,
         'headers': {
             'Content-Type': 'text/plain'
         },
         'body': f'Hello, CDK! You have hit {event["path"]}',
-        'array': array.tolist()
+        'array': arr.tolist()
     }
+
+def init_s3_client():
+    return s3_client
 
--- serverless/serverless_stack.py
+++ serverless/serverless_stack.py
@@ -1,6 +1,8 @@
 from aws_cdk import (
     core,
-    aws_lambda as _lambda
+    aws_lambda as _lambda,
+    aws_s3 as s3,
+    aws_s3_assets as s3_assets
 )
 
 
@@ -9,10 +11,16 @@ class ServerlessStack(core.Stack):
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
+        bucket: s3.Bucket = s3.Bucket(self, 'arium-serverless-data-bucket', block_public_access=s3.BlockPublicAccess.BLOCK_ALL, encryption=s3.BucketEncryption.S3_MANAGED)
+
         layer = _lambda.LayerVersion.from_layer_version_arn(self, 'AWSLambda-Python37-SciPy1x', 'arn:aws:lambda:us-east-1:668099181075:layer:AWSLambda-Python37-SciPy1x:22')
 
         # The code that defines your stack goes here
         my_lambda = _lambda.Function(self, 'HelloHandler', runtime=_lambda.Runtime.PYTHON_3_7,
-                                     code=_lambda.Code.asset('lambda'), handler='hello.handler')
+                                     code=_lambda.Code.asset('lambda'), handler='hello.handler', environment={'DATA_BUCKET' : bucket.bucket_name})
 
         my_lambda.add_layers(layer)
+
+        bucket.grant_read_write(my_lambda)
+
+
